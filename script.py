2ba115b5788ec1cfb9874c5f93d720c9b9d246fb
sthix <thanatosclothing@gmail.com>
2026-01-31 23:30:28 +0100

    Added first number-guessing logic

--- script.py
+++ script.py
@@ -1,7 +1,22 @@
+import random
 
 
 def guess_number():
-    pass
+    guess_range: int = int(input("How big is your guessing range? "))
+
+    random_number: int = random.randint(1, guess_range)
+
+    guess: int = int(input(f"Now guess the random number between 1 and {guess_range}! "))
+
+    if guess > random_number:
+        print("Smaller!")
+
+    elif guess < random_number:
+        print("Bigger!")
+
+    else:
+        print(f"You are right! The number was {random_number}")
 
 if __name__ == "__main__":
     guess_number()
+    pass
