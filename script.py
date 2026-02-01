2dc316c29f0db509f0d4e1e501b649e90d91c5a9
sthix <thanatosclothing@gmail.com>
2026-02-01 13:24:31 +0100

    No more while loop bugs

--- script.py
+++ script.py
@@ -2,20 +2,28 @@ import random
 
 
 def guess_number():
+    correctly_answer: bool = False
+    number_of_guesses: int = 0
+
     guess_range: int = int(input("How big is your guessing range? "))
 
     random_number: int = random.randint(1, guess_range)
 
     guess: int = int(input(f"Now guess the random number between 1 and {guess_range}! "))
 
-    if guess > random_number:
-        print("Smaller!")
+    while random_number != guess & correctly_answer == False:
+        if guess > random_number:
+            print("Smaller!")
+            guess: int = int(input("Guess again! "))
 
-    elif guess < random_number:
-        print("Bigger!")
+        elif guess < random_number:
+            print("Bigger!")
+            guess: int = int(input("Guess again! "))
 
-    else:
-        print(f"You are right! The number was {random_number}")
+        else:
+            correctly_answer = True
+            print(f"You are right. The number was {random_number}!")
+
 
 if __name__ == "__main__":
     guess_number()
