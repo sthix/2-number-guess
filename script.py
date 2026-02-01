87f8096618691d8348961bcba7b7e3da86490ca6
sthix <thanatosclothing@gmail.com>
2026-02-01 17:54:09 +0100

    Fix infinite while loop.

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ def guess_number():
     correctly_answer: bool = False
     number_of_guesses: int = 0
 
+    print("GUESS THE NUMBER!")
     guess_range: int = int(input("How big is your guessing range? "))
 
     random_number: int = random.randint(1, guess_range)
@@ -13,16 +14,20 @@ def guess_number():
 
     while random_number != guess & correctly_answer == False:
         if guess > random_number:
+            number_of_guesses += 1
             print("Smaller!")
             guess: int = int(input("Guess again! "))
 
         elif guess < random_number:
+            number_of_guesses += 1
             print("Bigger!")
             guess: int = int(input("Guess again! "))
 
         else:
+            number_of_guesses += 1
+            print(f"You are right. The number was {random_number}!\n"
+                  f"You needed {number_of_guesses} guesses.")
             correctly_answer = True
-            print(f"You are right. The number was {random_number}!")
 
 
 if __name__ == "__main__":
